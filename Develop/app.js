82d2f1ad0836aec14d6ba5011266d22c8d67f512
noobatl <info@noobusa.com>
2020-05-09 18:50:53 -0400

    edited .gitignore for lock.json file. Added inquirer questions for employee

--- Develop/app.js
+++ Develop/app.js
@@ -13,7 +13,77 @@ const render = require("./lib/htmlRenderer");
 ​
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-​
+// The first class is an `Employee` parent class with the following properties and methods:
+const employeeQuestions = [
+//   * name
+    {
+        type: 'input', 
+        name: 'name', 
+        message: `What is the employee's name?`
+    },
+//   * id
+    {
+        type: 'input',
+        name: 'id',
+        message: `What is the employee's id number?`
+    },
+//   * email
+    {
+        type: 'input',
+        name: 'email',
+        message: `What is the employee's email address?`
+    },
+//  * role
+    {
+        type: 'list',
+        name: 'role',
+        message: `What is the role of the employee?`,
+        choices: ['Manager', 'Engineer', 'Intern']
+    }
+// * getName()
+// * getId()
+// * getEmail()
+// * getRole() // Returns 'Employee'
+​];
+
+// In addition to `Employee`'s properties and methods, `Manager` will also have:
+const managerQuestions = 
+[
+//   * officeNumber
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: `What is the manager's office number?`
+    }
+//   * getRole() // Overridden to return 'Manager'
+];
+
+// In addition to `Employee`'s properties and methods, `Engineer` will also have:
+const engineerQuestions =
+[
+//   * github  // GitHub username
+    {
+        type: 'input',
+        name: 'github',
+        message: `What is the engineer's GitHub username?`
+    }
+//   * getGithub()
+//   * getRole() // Overridden to return 'Engineer'
+];
+
+// In addition to `Employee`'s properties and methods, `Intern` will also have:
+const internQuestions =
+[
+//   * school 
+    {
+        type: 'input',
+        name: 'school',
+        message: `What is the intern's school?`
+    }
+//   * getSchool()
+//   * getRole() // Overridden to return 'Intern'
+];
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
